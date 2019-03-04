5d89206c2e8e42d69f23d1eda0122517077dbebc
ujjwal0501 <31813384+Ujjwal0501@users.noreply.github.com>
2019-03-04 20:17:57 +0530

    keys binds

--- myscript.js
+++ myscript.js
@@ -1,17 +1,27 @@
-// function applyContrast() {
-// 	var list1 = document.getElementsByTagName('*'),
-// 	sp = localStorage.getItem('playspeed-chrome-extension-speed'),
-// 
-// 	if (sp == null) sp = "#333333";
-// 	if (txt == null) txt = "#ffffff";
-// 	if (border == null) border = "#222222";
-// 
-// 	for (var i = 0; i < list1.length; i++) {
-// 		list1[i].style.color = txt;
-// 		list1[i].style.backgroundColor = sp;
-// 		list1[i].style.borderColor = border;
-// 	}
-// 	console.log("done!");
-// }
-// 
-// window.onload = applyContrast();
+var video11 = document.getElementsByTagName('video')[0];
+var lspeed1 = localStorage['playspeed-chrome-extension-speed'];
+if (lspeed1 != null) video11.playbackRate = lspeed1;
+document.onkeydown = (keyid) => {
+    var speed11 = video11.playbackRate;
+    switch (keyid.keyCode) {
+        case 219:
+            // TODO: decrease
+            speed11 -= 0.1;
+            video11.playbackRate = (speed11>0.3?speed11:0.3);
+            localStorage['playspeed-chrome-extension-speed'] = speed11;
+            break;
+        case 221:
+            // TODO: increase
+            speed11 += 0.1;
+            video11.playbackRate = (speed11<8?speed11:8);
+            localStorage['playspeed-chrome-extension-speed'] = speed11;
+            break;
+        case 187:
+            // TODO: normal
+            video11.playbackRate = 1;
+            localStorage['playspeed-chrome-extension-speed'] = speed11;
+            break;
+        default:
+            break;
+    }
+}
